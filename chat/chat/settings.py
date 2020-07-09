928053e6215ca7b744510734f8c619eb75941617
zizdavova <60972789+zizdavova@users.noreply.github.com>
2020-07-09 15:16:45 +0300

    fgcgfhxcgfhxcfghfghfghпявапичсписпа

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -27,7 +27,7 @@ with open('key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
